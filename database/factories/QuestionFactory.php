51f19073ab5f3a2ce6015251535375754b714234
jamil <jamil.cse13@gmail.com>
2020-04-28 17:37:18 +0600

    fix foreign key error in questions table

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Question::class, function (Faker $faker) {
     return [
         'title' => rtrim($faker->sentence(rand(5, 10)), "."),
-        'body' => rtrim($faker->paragraphs(rand(5, 10)), "."),
+        'body' => $faker->paragraphs(rand(3, 7), true),
         'views' => rand(0,10),
         'answers' => rand(0,10),
         'votes' => rand(-3,10)
