26a657be652c26ce72a459f96effe8b3b908e9b4
jamil <jamil.cse13@gmail.com>
2020-05-02 02:43:13 +0600

    implement authorization using Gates

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,12 +36,18 @@
                                 <div class="d-flex align-items-center">
                                     <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
                                     <div class="ml-auto">
-                                        <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure want to delete?')">Delete</button>
-                                        </form>
+                                        
+                                        @if (Auth::user()->can('update-question', $question))
+                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endif
+
+                                        @if (Auth::user()->can('delete-question', $question))
+                                            <form class="form-delete" method="post" action="{{ route('questions.destroy', $question->id) }}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure want to delete?')">Delete</button>
+                                            </form>
+                                        @endif
                                     </div>
                                 </div>
                                 <p class="head">
