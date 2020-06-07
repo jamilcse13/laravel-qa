f0efe5321db37cf9017da5c9606dd718e2532565
jamil <jamil.cse13@gmail.com>
2020-06-08 02:31:32 +0600

    1. display all questions in root route (front page) 2. sort the answers by votes count 3. hide the answer's container when no answer 4. show an alert when question record empty

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -1,56 +1,58 @@
-<div class="row mt-4">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="card-title">
-                    <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
-                    <hr>
+@if ($answersCount > 0)
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ $answersCount . " " . str_plural('Answer', $answersCount) }}</h2>
+                        <hr>
 
-                    @include('layouts._message')
+                        @include('layouts._message')
 
-                    @foreach ($answers as $answer)
-                        <div class="media">
-                            
-                            @include('shared._vote', [
-                                'model' => $answer
-                            ])
+                        @foreach ($answers as $answer)
+                            <div class="media">
+                                
+                                @include('shared._vote', [
+                                    'model' => $answer
+                                ])
 
-                            <div class="media-body">
-                                {!! $answer->body_html !!}
+                                <div class="media-body">
+                                    {!! $answer->body_html !!}
 
-                                {{-- edit and delete options --}}
-                                <div class="row">
-                                    <div class="col-4">
-                                        <div class="ml-auto">
-                                            {{-- @if (Auth::user()->can('update', $question)) --}}
-                                            @can ('update', $answer)
-                                                <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                            @endcan
-    
-                                            @can ('delete', $answer)
-                                                <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
-                                                    @method('DELETE')
-                                                    @csrf
-                                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure want to delete?')">Delete</button>
-                                                </form>
-                                            @endcan
+                                    {{-- edit and delete options --}}
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <div class="ml-auto">
+                                                {{-- @if (Auth::user()->can('update', $question)) --}}
+                                                @can ('update', $answer)
+                                                    <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                                @endcan
+        
+                                                @can ('delete', $answer)
+                                                    <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
+                                                        @method('DELETE')
+                                                        @csrf
+                                                        <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure want to delete?')">Delete</button>
+                                                    </form>
+                                                @endcan
+                                            </div>
+                                        </div>
+                                        <div class="col-4"></div>
+                                        <div class="col-4">
+                                            @include('shared._author', [
+                                                'model' => $answer,
+                                                'label' => 'Answered'
+                                            ])
                                         </div>
                                     </div>
-                                    <div class="col-4"></div>
-                                    <div class="col-4">
-                                        @include('shared._author', [
-                                            'model' => $answer,
-                                            'label' => 'Answered'
-                                        ])
-                                    </div>
-                                </div>
 
+                                </div>
                             </div>
-                        </div>
-                        <hr>
-                    @endforeach
+                            <hr>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+@endif
