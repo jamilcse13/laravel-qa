1f048da122ebce35d25c17425b9489760dd62622
jamil <jamil.cse13@gmail.com>
2020-06-07 18:39:10 +0600

    securing the application from xss attack

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -56,7 +56,7 @@
                                     <small class="text-muted">{{ $question->created_date }}</small>
                                 </p>
 
-                                {{ str_limit($question->body, 250) }}
+                                <div class="excerpt">{{ $question->excerpt(350) }}</div>
                             </div>
                         </div>
                         <hr>
